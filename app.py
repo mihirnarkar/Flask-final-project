7ea4e56262cf72daedab0233918f009d8eacce6a
Mihir Narkar <Mihirnarkar19@gmail.com>
2022-11-10 13:00:41 +0530

    database of contact page added

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:@localhost/cakeshopweb-app"
+db = SQLAlchemy(app)
+
+class Contact(db.Model):
+    userid = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False)
+    useremail = db.Column(db.String(120), unique=True, nullable=False)
+    usersubject = db.Column(db.String(300), nullable=False)
+    usermessage = db.Column(db.String(300), nullable=False)
 
 # defining a route
 @app.route("/") # decorator
@@ -11,8 +21,19 @@ def home():
 def about():
     return render_template('about.html')
 
-@app.route("/contact") 
+@app.route("/contact",methods=['GET','POST']) 
 def contact():
+    if (request.method=='POST'):
+        name = request.form.get('name')
+        email = request.form.get('email')
+        subject = request.form.get('subject')
+        message = request.form.get('message')
+
+        entry = Contact(username=name,useremail=email,usersubject=subject,usermessage=message)
+        db.session.add(entry)
+        db.session.commit()
+
+
     return render_template('contact.html')
 
 @app.route("/menu") 
